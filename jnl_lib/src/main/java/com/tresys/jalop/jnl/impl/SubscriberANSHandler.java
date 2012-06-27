56277341ab0e6e09574824331de1121eb4919802
Michael Napolitano <mnapolitano@tresys.com>
2012-06-27 06:35:16 -0700

    bug-16319: Implement A wrapper around the beepcore inputstream

--- jnl_lib/src/main/java/com/tresys/jalop/jnl/impl/SubscriberANSHandler.java
+++ jnl_lib/src/main/java/com/tresys/jalop/jnl/impl/SubscriberANSHandler.java
@@ -4,7 +4,7 @@
  *
  * All other source code is copyright Tresys Technology and licensed as below.
  *
- * Copyright (c) ${year} Tresys Technology LLC, Columbia, Maryland, USA
+ * Copyright (c) 2012 Tresys Technology LLC, Columbia, Maryland, USA
  *
  * This software was developed by Tresys Technology LLC
  * with U.S. Government sponsorship.
@@ -23,23 +23,45 @@
  */
 package com.tresys.jalop.jnl.impl;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import javax.xml.soap.MimeHeader;
 
+import org.beepcore.beep.core.AbortChannelException;
+import org.beepcore.beep.core.BEEPException;
 import org.beepcore.beep.core.InputDataStream;
+import org.beepcore.beep.core.InputDataStreamAdapter;
+import org.beepcore.beep.core.Message;
+import org.beepcore.beep.core.ReplyListener;
 
 import com.tresys.jalop.jnl.IncompleteRecordException;
+import com.tresys.jalop.jnl.RecordInfo;
+import com.tresys.jalop.jnl.RecordType;
+import com.tresys.jalop.jnl.Subscriber;
+import com.tresys.jalop.jnl.exceptions.UnexpectedMimeValueException;
+import com.tresys.jalop.jnl.impl.messages.Utils;
+import com.tresys.jalop.jnl.impl.subscriber.SubscriberSessionImpl;
 
 /**
  * This class reads the ANS reply for a 'journal-resume' or 'subscribe' message
  * and can be used to present the various data segments as
  * {@link java.io.InputStream}s.
  */
-public class SubscriberANSHandler {
+public class SubscriberANSHandler implements ReplyListener {
+	
+	private static final String[] KNOWN_HEADERS = { Utils.HDRS_APP_META_LEN.toLowerCase(),
+			Utils.HDRS_SYS_META_LEN.toLowerCase(), Utils.HDRS_LOG_LEN.toLowerCase(), Utils.HDRS_AUDIT_LEN.toLowerCase(), Utils.HDRS_JOURNAL_LEN.toLowerCase(),
+			Utils.HDRS_CONTENT_TYPE.toLowerCase(), Utils.HDRS_MESSAGE.toLowerCase(), Utils.HDRS_SERIAL_ID.toLowerCase() };
 	/**
-	 * The MessageDigset to use for calculating the JALoP digest.
+	 * The MessageDigest to use for calculating the JALoP digest.
 	 */
 	private MessageDigest md;
 	/**
@@ -48,52 +70,146 @@ public class SubscriberANSHandler {
 	private InputDataStream ds;
 
 	/**
+	 * A boolean indicating whether the payload size was what was expected. This
+	 * is used so that MessageDigest is able to know whether or not to send an
+	 * IncompleteRecordException
+	 */
+	private boolean payloadCorrect;
+	private boolean payloadComplete;
+
+	private JalopDataStream js;
+	private SubscriberSessionImpl subsess;
+	static Logger log = Logger.getLogger(SubscriberANSHandler.class);
+
+	/**
 	 * Create a SubscriberANSHandler for a record using the
 	 * {@link InputDataStream}
-	 * 
+	 *
 	 * @param md
 	 *            the {@link MessageDigest} that should be used for digest
 	 *            calculations.
 	 * @param ds
 	 *            the {@link InputDataStream} to read
 	 */
-	public SubscriberANSHandler(MessageDigest md, InputDataStream ds) {
+	public SubscriberANSHandler(MessageDigest md,
+			SubscriberSessionImpl subsess, InputDataStream ds) {
 		super();
 		this.md = md;
 		this.ds = ds;
+		this.subsess = subsess;
+		this.payloadCorrect = true;
+		this.payloadComplete = false;
 	}
 
-	/**
-	 * Retrieve an {@link InputStream} for the system meta-data.
-	 * 
-	 * @return an {@link InputStream}, or NULL if the system meta-data size is
-	 *         0.
-	 */
-	public InputStream getSystemMetadata() {
-		// TODO: Implement this method.
-		return null;
+	private class JalopDataStream extends InputStream {
+		private boolean finishedReading;
+		InputDataStreamAdapter dsa;
+		private long dataSize;
+		private long bytesRead;
+
+		public JalopDataStream(long dataSize) throws IOException,
+				BEEPException, UnexpectedMimeValueException {
+			this.finishedReading = false;
+			this.dsa = ds.getInputStream();
+			this.bytesRead = 0;
+			if (dataSize < 0) {
+				throw new IllegalArgumentException(
+						"dataSize must be 0 or greater");
+			}
+			this.dataSize = dataSize;
+		}
+
+		@Override
+		public int read() throws IOException {
+			if (this.finishedReading == true)
+				return -1;
+
+			int ret = 0;
+			// only read if data is expected
+			if (this.dataSize != 0) {
+				ret = this.dsa.read();
+				if (ret == -1) {
+					throw new IOException();
+				}
+
+				md.update((byte) ret);
+				this.bytesRead++;
+			}
+
+			if (this.bytesRead == this.dataSize) {
+				// we should be done. Try to read the BREAK string
+				byte b[] = new byte[5];
+				this.dsa.read(b, 0, 5);
+				if (!Arrays.equals("BREAK".getBytes("utf-8"), b)) {
+					payloadCorrect = false;
+					throw new IOException("BREAK string is not where it is expected");
+				} else {
+					payloadCorrect = true;
+				}
+				this.finishedReading = true;
+			}
+			return ret;
+		}
+
+		@Override
+		public int read(byte[] b) throws IOException {
+			return read(b, 0, b.length);
+		}
+
+		@Override
+		public int read(byte[] b, int off, int len) throws IOException {
+			if (this.finishedReading == true)
+				return -1;
+
+			int ret = 0;
+			int new_len = len;
+			if ((len + this.bytesRead) > this.dataSize) {
+				new_len = (int) (this.dataSize - this.bytesRead);
+			}
+			
+			ret = this.dsa.read(b, off, new_len);
+			this.bytesRead+=ret;
+			if (ret != new_len) {
+				throw new IOException(
+						"Could not read data of requested length");
+			}
+			md.update(b, off, new_len);
+			if (this.bytesRead == this.dataSize) {
+				// check for break string
+				byte brk[] = new byte[5];
+				this.dsa.read(brk, 0, 5);
+				if (!Arrays.equals("BREAK".getBytes("utf-8"), brk)) {
+					payloadCorrect = false;
+					throw new IOException("BREAK string is not where it is expected");
+				} else {
+					payloadCorrect = true;
+				}
+				this.finishedReading = true;
+			}
+			return ret;
+		}
+
+		public void flush() throws IOException {
+			if (!this.finishedReading) {
+				int ret = read();
+				while (ret != -1) {
+					ret = read();
+				}
+			}
+
+			return;
+		}
 	}
 
-	/**
-	 * Retrieve an {@link InputStream} for the application meta-data.
-	 * 
-	 * @return an {@link InputStream}, or NULL if the system meta-data size is
-	 *         0.
+	/*
+	 * This function is only used for testing purposes. It returns a {@link
+	 * JalopDataStream}
+	 *
+	 * @param size the size to give the instance of JalopDataStream
 	 */
-	public InputStream getAppMetadata() {
-		// TODO: Implement this method.
-		return null;
-	}
-
-	/**
-	 * Retrieve an {@link InputStream} for the payload (journal, audit, or log
-	 * data).
-	 * 
-	 * @return an {@link InputStream}, or NULL if there is no payload.
-	 */
-	public InputStream getPayload() {
-		// TODO: Implement this method.
-		return null;
+	JalopDataStream getJalopDataStreamInstance(long size)
+			throws UnexpectedMimeValueException, IOException, BEEPException {
+		return new JalopDataStream(size);
 	}
 
 	/**
@@ -102,17 +218,27 @@ public class SubscriberANSHandler {
 	 * JALoP headers) are not included in the returned map. If there are no
 	 * additional MIME headers, then this function returns {@link MimeHeaders}
 	 * object.
-	 * 
+	 *
 	 * @return The {@link MimeHeaders}
+	 * @throws BEEPException
 	 */
-	public MimeHeader getAddtionalHeaders() {
-		// TODO: implement this method.
-		return null;
+	@SuppressWarnings("unchecked")
+	public List<MimeHeader> getAdditionalHeaders() throws BEEPException {
+		InputDataStreamAdapter dsa = this.ds.getInputStream();
+		Enumeration<String> headers = dsa.getHeaderNames();
+		ArrayList<MimeHeader> ret = new ArrayList<MimeHeader>();
+		String hdr = "";
+		while (headers.hasMoreElements()) {
+			hdr = headers.nextElement();
+			if (!Arrays.asList(KNOWN_HEADERS).contains(hdr.toLowerCase()))
+				ret.add(new MimeHeader(hdr, dsa.getHeaderValue(hdr)));
+		}
+		return ret;
 	}
 
 	/**
 	 * Retrieve the calculated digest.
-	 * 
+	 *
 	 * @return The digest of the record according to the JALoP specification.
 	 * @throws IncompleteRecordException
 	 *             is thrown if this method is called before reading all data
@@ -121,7 +247,147 @@ public class SubscriberANSHandler {
 	 *             only contains 50).
 	 */
 	public byte[] getRecordDigest() throws IncompleteRecordException {
-		// TODO: implement this method.
-		return null;
+		if (!this.payloadComplete || !this.payloadCorrect)
+			throw new IncompleteRecordException();
+
+		return this.md.digest();
+	}
+
+	@Override
+	public void receiveRPY(Message message) throws AbortChannelException {
+
+		if (log.isEnabledFor(Level.ERROR)) {
+			log.error("SubscriberANSHandler received RPY, which should not happen");
+		}
+
+		throw new AbortChannelException(
+				"SubscriberANSHandler should not receive RPY");
+
+	}
+
+	@Override
+	public void receiveERR(Message message) throws AbortChannelException {
+
+		if (log.isEnabledFor(Level.ERROR)) {
+			log.error("SubscriberANSHandler received ERR");
+		}
+		throw new AbortChannelException("SubscriberANSHandler received ERR");
+
+	}
+
+	@Override
+	public void receiveANS(Message message) throws AbortChannelException {
+		this.ds = message.getDataStream();
+		final InputDataStreamAdapter dsa = this.ds.getInputStream();
+		final long sysMetadataSize;
+		final long appMetadataSize;
+		final long payloadSize;
+		final String payloadType;
+		final RecordType recType;
+		// Get the segment lengths from the header
+		try {
+			sysMetadataSize = new Long(dsa
+					.getHeaderValue(Utils.HDRS_SYS_META_LEN)).longValue();
+			appMetadataSize = new Long(dsa.getHeaderValue(Utils.HDRS_APP_META_LEN)).longValue();
+
+			payloadType = dsa.getHeaderValue(Utils.HDRS_MESSAGE);
+			if (payloadType.equalsIgnoreCase("log-record")) {
+				payloadSize = new Long(dsa.getHeaderValue(Utils.HDRS_LOG_LEN)).longValue();
+				recType = RecordType.Log;
+			} else if (payloadType.equalsIgnoreCase("audit-record")) {
+				payloadSize = new Long(dsa.getHeaderValue(Utils.HDRS_AUDIT_LEN)).longValue();
+				recType = RecordType.Audit;
+			} else if (payloadType.equalsIgnoreCase("journal-record")) {
+				payloadSize = new Long(dsa.getHeaderValue(Utils.HDRS_JOURNAL_LEN)).longValue();
+				recType = RecordType.Journal;
+			} else {
+				throw new AbortChannelException(
+						"Did not receive appropriate headers");
+			}
+		} catch (BEEPException e) {
+			throw new AbortChannelException(e.getMessage());
+		}
+
+		Subscriber sub = this.subsess.getSubscriber();
+		try {
+			RecordInfo recInfo = new RecordInfo() {
+
+				private long sysMetadataLength = sysMetadataSize;
+				private long appMetadataLength = appMetadataSize;
+				private long payloadLength = payloadSize;
+				private String serialId = dsa.getHeaderValue(Utils.HDRS_SERIAL_ID);
+				private RecordType recordType = recType;
+
+				@Override
+				public long getSysMetaLength() {
+					return this.sysMetadataLength;
+				}
+
+				@Override
+				public String getSerialId() {
+					return this.serialId;
+				}
+
+				@Override
+				public RecordType getRecordType() {
+					return this.recordType;
+				}
+
+				@Override
+				public long getPayloadLength() {
+					return this.payloadLength;
+				}
+
+				@Override
+				public long getAppMetaLength() {
+					return this.appMetadataLength;
+				}
+			};
+
+			js = new JalopDataStream(sysMetadataSize);
+			if (!sub.notifySysMetadata(subsess, recInfo, js)) {
+				throw new AbortChannelException("Error in notifySysMetadata");
+			}
+			js.flush();
+
+			js = new JalopDataStream(appMetadataSize);
+			if (!sub.notifyAppMetadata(subsess, recInfo, js)) {
+				throw new AbortChannelException("Error in notifyAppMetadata");
+			}
+			js.flush();
+
+			js = new JalopDataStream(payloadSize);
+			if (!sub.notifyPayload(subsess, recInfo, js)) {
+				throw new AbortChannelException("Error in notifyPayload");
+			}
+			js.flush();
+
+			if (ds.getInputStream().read() != -1) {
+				throw new IOException(
+						"Additional data exists when none is expected");
+			}
+			payloadComplete = true;
+			if (!sub.notifyDigest(subsess, recInfo, getRecordDigest())) {
+				throw new AbortChannelException("Error in notifyDigest");
+			}
+		} catch (BEEPException e) {
+			throw new AbortChannelException(e.getMessage());
+		} catch (UnexpectedMimeValueException e) {
+			throw new AbortChannelException(e.getMessage());
+		} catch (IOException e) {
+			throw new AbortChannelException(e.getMessage());
+		} catch (IncompleteRecordException e) {
+			throw new AbortChannelException(e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void receiveNUL(Message message) throws AbortChannelException {
+		if (log.isEnabledFor(Level.ERROR)) {
+			log.error("SubscriberANSHandler received NUL");
+		}
+		throw new AbortChannelException(
+				"SubscriberANSHandler should not receive NUL");
 	}
 }
