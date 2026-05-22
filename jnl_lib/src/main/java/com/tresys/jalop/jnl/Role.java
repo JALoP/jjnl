05805738aaaeeaec593f879a612f5af8964e2bc3
Mollick A. Arefin <mollick.arefin@cdsc.ctc.com>
2026-05-22 13:09:39 -0400

    Updated jnl_lib from intenal dev branch.

--- jnl_lib/src/main/java/com/tresys/jalop/jnl/Role.java
+++ jnl_lib/src/main/java/com/tresys/jalop/jnl/Role.java
@@ -1,13 +1,5 @@
-/*
- * Source code in 3rd-party is licensed and owned by their respective
- * copyright holders.
- *
- * All other source code is copyright Tresys Technology and licensed as below.
- *
- * Copyright (c) 2012 Tresys Technology LLC, Columbia, Maryland, USA
- *
- * This software was developed by Tresys Technology LLC
- * with U.S. Government sponsorship.
+/**
+ * Copyright (C) 2026 Concurrent Technologies Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
 
 package com.tresys.jalop.jnl;
 
--- jnl_lib/src/main/java/com/tresys/jalop/jnl/impl/DigestAlgorithms.java
+++ jnl_lib/src/main/java/com/tresys/jalop/jnl/impl/DigestAlgorithms.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2023 The National Security Agency (NSA)
+/**
+ * Copyright (C) 2026 Concurrent Technologies Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
 
 package com.tresys.jalop.jnl.impl;
 
